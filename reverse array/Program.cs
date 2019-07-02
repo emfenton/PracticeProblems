d691fbe56a8809bd6f0996ea8bf174adb8c9fe36
Erin M Fenton <erinmfenton@gmail.com>
2019-07-02 08:37:15 -0700

    reverses an array

---
+++ reverse array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace reverse_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] myArray = new int[] {1, 3, 5, 7, 9, 2, 6, 22, 4, 7};
+
+            for ( int i = myArray.Length - 1; i >= myArray[0] - 1 ; i--)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
