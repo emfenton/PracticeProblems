cfab8f29669e3e438189b24e17a0f400c632ff05
Erin M Fenton <erinmfenton@gmail.com>
2019-07-02 08:23:12 -0700

    finds the largest number in an array

---
+++ largest number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace largest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] myArray = new int[] {1, 3, 5, 7, 9, 2, 6, 22, 4, 7};
+            int largestNumber = int.MinValue;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] > largestNumber)
+                {
+                    largestNumber = myArray[i];
+                }
+            }
+            Console.WriteLine(largestNumber);
+        }
+    }
+}
